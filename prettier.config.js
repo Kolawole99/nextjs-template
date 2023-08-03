ae00f53ce9780fb2c7786190a9e2d3213c92659a
Kolawole Ojo <philliphopeojo@gmail.com>
2023-08-03 09:41:41 +0100

    configuring app rules and settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   plugins: [
-    'prettier-plugin-tailwindcss' // MUST come last
+    'prettier-plugin-tailwindcss', // MUST come last
   ],
-  pluginSearchDirs: ['.'],
   semi: true,
-  trailingComma: 'none',
   singleQuote: true,
   printWidth: 120,
   tabWidth: 2,
   arrowParens: 'avoid',
-  bracketSpacing: true
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  trailingComma: 'es5',
+  useTabs: false,
 };
