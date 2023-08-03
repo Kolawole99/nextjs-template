b82586cf0d3e9249a43677959f2cb81538b61568
Kolawole Ojo <philliphopeojo@gmail.com>
2023-08-03 09:00:01 +0100

    Adding configurations for rules

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    'prettier-plugin-tailwindcss' // MUST come last
+  ],
+  pluginSearchDirs: ['.'],
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  bracketSpacing: true
+};
